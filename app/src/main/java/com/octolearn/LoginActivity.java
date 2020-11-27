5ab977f3c1894bffdb18e84d21ef16601317b971
Klaudia Teodorowicz <klaudiateodorowicz@gmail.com>
2020-11-27 15:25:50 +0100

    LoginActivity connection with MainActivity

--- app/src/main/java/com/octolearn/LoginActivity.java
+++ app/src/main/java/com/octolearn/LoginActivity.java
@@ -2,7 +2,11 @@ package com.octolearn;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -10,5 +14,22 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        Button button = (Button) findViewById(R.id.button);
+        TextView signup = (TextView) findViewById(R.id.textView6);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(LoginActivity.this, MainActivity.class));
+            }
+        });
+
+        signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(LoginActivity.this, RegisterActivity.class));
+            }
+        });
+
     }
 }
--- app/src/main/java/com/octolearn/RegisterActivity.java
+++ app/src/main/java/com/octolearn/RegisterActivity.java
@@ -2,7 +2,11 @@ package com.octolearn;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -10,5 +14,22 @@ public class RegisterActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        Button button = (Button) findViewById(R.id.button);
+        TextView signin = (TextView) findViewById(R.id.textView6);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(RegisterActivity.this, MainActivity.class));
+            }
+        });
+
+        signin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(RegisterActivity.this, LoginActivity.class));
+            }
+        });
     }
 }
