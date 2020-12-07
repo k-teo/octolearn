264b6b063634fab21eec6608c0ab551bb349935d
MKDMK <dams.michal@onet.pl>
2020-12-07 12:46:18 +0100

    Navigation menu redesign, added new icons

--- app/src/main/java/com/octolearn/MainActivity.java
+++ app/src/main/java/com/octolearn/MainActivity.java
@@ -2,57 +2,87 @@ package com.octolearn;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.Menu;
 import android.widget.Button;
 import android.widget.Toast;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.navigation.NavigationView;
 import com.google.firebase.auth.FirebaseAuth;
 
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
-import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
-//próba mikrofonu
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-    private AppBarConfiguration mAppBarConfiguration;
+public class MainActivity extends AppCompatActivity implements View.OnClickListener, NavigationView.OnNavigationItemSelectedListener {
+
+    private DrawerLayout drawer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        drawer = findViewById(R.id.drawer_layout);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar,
+                                                        R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
         FloatingActionButton fab = findViewById(R.id.fab);
-
-        Button button1 = findViewById(R.id.button1);
-        Button button2 = findViewById(R.id.button2);
-        Button button3 = findViewById(R.id.button3);
-
         fab.setOnClickListener(this);
-        button1.setOnClickListener(this);
-        button2.setOnClickListener(this);
-        button3.setOnClickListener(this);
 
-        DrawerLayout drawer = findViewById(R.id.drawer_layout);
         NavigationView navigationView = findViewById(R.id.nav_view);
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        mAppBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow)
-                .setDrawerLayout(drawer)
-                .build();
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
-        NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
-        NavigationUI.setupWithNavController(navigationView, navController);
+        navigationView.setNavigationItemSelectedListener(this);
+    }
+
+    @Override
+    public void onBackPressed(){
+        if (drawer.isDrawerOpen(GravityCompat.START)){
+            drawer.closeDrawer(GravityCompat.START);
+        } else{
+            super.onBackPressed();
+        }
+        super.onBackPressed();
+    }
+
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.nav_import:
+                startActivity(new Intent(getApplicationContext(), CreateFlashcardActivity.class));
+                break;
+            case R.id.nav_export:
+                startActivity(new Intent(getApplicationContext(), CreateCatalogActivity.class));
+                break;
+            case R.id.nav_statistics:
+                startActivity(new Intent(getApplicationContext(), CreateCatalogActivity.class));
+                break;
+            case R.id.nav_friends:
+                startActivity(new Intent(getApplicationContext(), CreateCatalogActivity.class));
+                break;
+            case R.id.nav_settings:
+                startActivity(new Intent(getApplicationContext(), CreateCatalogActivity.class));
+                break;
+            case R.id.nav_profile:
+                startActivity(new Intent(getApplicationContext(), CreateCatalogActivity.class));
+                break;
+            case R.id.nav_logout:
+                logout();
+                break;
+
+        }
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
     }
 
     @Override
@@ -60,17 +90,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         switch(v.getId()){
             case R.id.fab:
                 startActivity(new Intent(MainActivity.this, CreateCatalogActivity.class));
-                break;
-            case R.id.button1:
-                startActivity(new Intent(MainActivity.this, WayOfLearningActivity.class));
-                break;
-            case R.id.button2:
-                Toast.makeText(this,"Not assigned.", Toast.LENGTH_SHORT).show();
-                //startActivity(new Intent(MainActivity.this, .class));
-                break;
-            case R.id.button3:
-                Toast.makeText(this,"Not assigned.", Toast.LENGTH_SHORT).show();
-                //startActivity(new Intent(MainActivity.this, .class));
                 break;
         }
     }
@@ -82,14 +101,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         return true;
     }
 
-    @Override
-    public boolean onSupportNavigateUp() {
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
-        return NavigationUI.navigateUp(navController, mAppBarConfiguration)
-                || super.onSupportNavigateUp();
-    }
-
-    public void logout(View view){
+    public void logout(){
         FirebaseAuth.getInstance().signOut();
         startActivity(new Intent(MainActivity.this, LoginActivity.class));
         finish();
